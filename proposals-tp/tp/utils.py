13f8bcfaa3418fd3733e71b9caecd87f572c56e0
Anna <anjankow@protonmail.com>
2022-06-04 12:04:44 -0400

    add util func reading settings

--- proposals-tp/tp/utils.py
+++ proposals-tp/tp/utils.py
@@ -18,6 +18,52 @@
 
 import cbor
 from sawtooth_sdk.processor.exceptions import InternalError
+from .setting_pb2 import Setting
+import logging
+import hashlib
+
+LOGGER = logging.getLogger(__name__)
+
+SETTINGS_NAMESPACE = '000000'
+_SETTINGS_MAX_KEY_PARTS = 4
+_SETTINGS_ADDRESS_PART_SIZE = 16
+
+
+def get_setting_addr(key: str) -> str:
+    # source: https://github.com/hyperledger/sawtooth-core/blob/6092b48b4de3498df0416c6ccf2f81f4b5468995/cli/sawtooth_cli/settings.py#L150
+    """Creates the state address for a given setting key.
+    """
+    key_parts = key.split('.', maxsplit=_SETTINGS_MAX_KEY_PARTS - 1)
+    key_parts.extend([''] * (_SETTINGS_MAX_KEY_PARTS - len(key_parts)))
+
+    def _short_hash(in_str):
+        return hashlib.sha256(in_str.encode()).hexdigest()[:_SETTINGS_ADDRESS_PART_SIZE]
+
+    return SETTINGS_NAMESPACE + ''.join(_short_hash(x) for x in key_parts)
+
+
+def read_setting(context, key: str, defaultValue):
+    state = context.get_state([get_setting_addr(key)])
+    data = state[0].data
+
+    set = Setting()
+    set.ParseFromString(data)
+
+    if len(set.entries) < 1:
+        LOGGER.info('setting missing: '+key+', using default value: ' +
+                    str(defaultValue))
+
+        return defaultValue
+
+    if set.entries[0].key != key:
+        LOGGER.warning('read unexpected setting: ' +
+                       set.entries[0].key+', expected: '+key)
+        return defaultValue
+
+    setValue = set.entries[0].value
+    LOGGER.debug('read setting: ' + key + '=' + str(setValue))
+
+    return setValue
 
 
 def get_state_data(context, address):
